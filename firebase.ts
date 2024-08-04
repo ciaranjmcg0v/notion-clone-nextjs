5cdf794405b43660c2cccbf368028d62acde8039
Ciaran McGovern <ciaranjmcgovern@gmail.com>
2024-08-04 15:05:31 +0100

    Updated firebase.ts to read firebase config from .env

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,7 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBAPy2YBhXMTMPAf1ECp82Yo8FWvm45oos",
-  authDomain: "notion-clone-863c7.firebaseapp.com",
-  projectId: "notion-clone-863c7",
-  storageBucket: "notion-clone-863c7.appspot.com",
-  messagingSenderId: "419390231217",
-  appId: "1:419390231217:web:c0ed80fed5ad3e9e4bc890",
-  measurementId: "G-YG8JFJY9DE",
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
